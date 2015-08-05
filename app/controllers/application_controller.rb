12e10fb76930c15272180462ab444672fb323c22
Lily Parmenter <parmenter.lily@gmail.com>
2015-08-05 18:48:02 -0500

    Redirects to show user profile on signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
   end
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
 end
